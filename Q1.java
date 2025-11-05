36611a3584d8a2e8e31f15716dc030fd65e6d695
Kunika Agrawal <100677069+agrawalkunika@users.noreply.github.com>
2025-11-05 20:31:53 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.err.println("Print the first number");
+        int a = sc.nextInt();
+        System.err.println("Print the Second number");
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.err.println(sum);
+
+    }
+}
